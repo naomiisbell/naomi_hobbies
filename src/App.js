7c8afff10491f62ebd2742b836018c53fb998b18
Naomi Isbell <naomiisbell@gmail.com>
2020-07-12 08:45:09 -0400

    css and js updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import HobbyOne from './components/HobbyOne';
+import HobbyTwo from './components/HobbyTwo';
+import HobbyThree from './components/HobbyThree';
+import Item1 from './assets/writingimg.jpg'
+import Item2 from './assets/actingimg.jpg'
+import Item3 from './assets/programmingimg.jpeg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hobbyOne: 'Writing',
+      hobbyTwo: 'Acting',
+      hobbyThree: 'Programming'
+  }
+}
+
+    render() {
+    return (
+      <div className="App">
+        <h1>My Hobbies</h1>
+        <HobbyOne
+        hobbyOne={this.state.hobbyOne}
+        />
+        <img src={Item1}></img>
+        <p>My first favorite hobby is to write. I love to take myself into a completely different world and create new stories with characters like me. It is something i've recently discovered that I enjoy but am also not terrible at. I would really like to combine my love for writing and acting one day and turn it into a film. </p>
+        
+        <HobbyTwo
+        hobbyTwo={this.state.hobbyTwo}
+        />
+         <img src={Item2}></img>
+        <HobbyThree
+        hobbyThree={this.state.hobbyThree}
+        />
+         <img src={Item3}></img>
+      </div>
+    );
+  }
 }
 
 export default App;
